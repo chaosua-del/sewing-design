1801bb5634a905b8ef9d9eefaae301d77503937d
chaosua-del <d.yuhymenko@gmail.cpm>
2020-08-22 11:48:16 +0300

    fix: fixed uniforms__button animation

--- src/js/banner-hover.js
+++ src/js/banner-hover.js
@@ -1,26 +1,52 @@
+import $ from 'jquery';
+
 export default function bannerHover() {
-  // banner hovers
-  if (window.innerWidth >= 1250) {
+    // banner hovers
+    if (window.innerWidth >= 1250) {
 
-    let bannerWrap = document.querySelectorAll('.banner__wrap');
-    let bannerSlide = document.querySelector('.banner__swiper-slide');
-    bannerWrap.forEach((elem) => {
-      elem.addEventListener('mouseenter', (event) => {
-        event.target.classList.add('banner__wrap--active');
-        if (event.clientX > window.innerWidth / 2) {
-          bannerSlide.style.transform = 'translateX(-30%)';
-        }
-        else if (bannerSlide.style.transform === 'translateX(-30%)') {
-          console.log('hello');
-          bannerSlide.style.transform = 'translateX(0%)';
-        }
+        let bannerWrap = document.querySelectorAll('.banner__wrap');
+        let bannerSlide = document.querySelector('.banner__swiper-slide');
+        bannerWrap.forEach((elem) => {
+            elem.addEventListener('mouseenter', (event) => {
+                event.target.classList.add('banner__wrap--active');
+                if (event.clientX > window.innerWidth / 2) {
+                    let counter = parseInt($(event.target).css("margin-left"));
+                    console.log(counter);
+                    let interval = setInterval(() => {
+                        if (counter <= -(parseInt($(event.target).css("width"))) / 1.8)
+                            clearInterval(interval);
+                        else {
+                            counter -= 10;
+                            event.target.style.marginLeft = `${counter}px`;
+                        }
 
-      });
-      elem.addEventListener('mouseleave', event => {
-        event.target.classList.remove('banner__wrap--active');
-      });
-    });
+                    }, 15);
+                    event.target.addEventListener('mouseleave', function (event) {
+                        event.target.classList.remove('banner__wrap--active');
+                        clearInterval(interval);
+                    });
+                } else if (event.clientX < window.innerWidth / 2) {
+                    let counter = parseInt($(event.target).css("margin-left"));
+                    let interval = setInterval(() => {
+                        if (counter >= 0)
+                            clearInterval(interval);
+                        else {
+                            counter += 10;
+                            event.target.style.marginLeft = `${counter}px`;
+                        }
+
+                    }, 15);
+                    event.target.addEventListener('mouseleave', function (event) {
+                        event.target.classList.remove('banner__wrap--active');
+                        clearInterval(interval);
+                    });
+                }
+
+            });
+        });
 
 
-  }
+    }
+
+
 }
