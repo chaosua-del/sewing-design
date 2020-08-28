b78d9b8729dfae499b9709eff6d299f41dbfd5da
Yuhymenko Dmytro <d.yuhymenko@riverstart.ru>
2020-08-28 15:23:01 +0300

    feat: modal added

--- src/js/model-expand.js
+++ src/js/model-expand.js
@@ -1,32 +1,32 @@
 export default function modelExpand() {
-  let text = {
-    neck: 'neck',
-    shoulder: 'shouler'
-  }
-  let expandButton = document.querySelectorAll('.model__button');
-  expandButton.forEach((elem) => {
-    elem.addEventListener('click', function () {
-      if (window.innerWidth >= 768) {
-        console.log('hello');
-        if (event.target.classList.contains('button--close')) {
-          event.target.classList.remove('button--expand', 'button--close');
-          event.target.innerHTML = '+';
-          // console.log(event.target);
-        }
-        else {
-          expandButton.forEach((elem) => {
-            if (elem != event.target) {
-              elem.classList.remove('button--expand', 'button--close');
-              elem.innerHTML = '+';
+    let text = {
+        neck: 'neck',
+        shoulder: 'shouler'
+    }
+    let expandButton = document.querySelectorAll('.model__button');
+    expandButton.forEach((elem) => {
+        elem.addEventListener('click', function () {
+            if (window.innerWidth >= 768) {
+                // console.log('hello');
+                if (event.target.classList.contains('button--close')) {
+                    event.target.classList.remove('button--expand', 'button--close');
+                    event.target.innerHTML = '+';
+                    // console.log(event.target);
+                } else {
+                    expandButton.forEach((elem) => {
+                        if (elem != event.target) {
+                            elem.classList.remove('button--expand', 'button--close');
+                            elem.innerHTML = '+';
+                        }
+
+                    });
+                    event.target.classList.add('button--expand');
+                    event.target.classList.add('button--close');
+                    // .style.display = 'block';
+                    event.target.innerHTML = ``;
+                }
             }
 
-          });
-          event.target.classList.add('button--expand');
-          event.target.classList.add('button--close');
-          event.target.innerHTML = ``;
-        }
-      }
-
+        });
     });
-  });
 }
